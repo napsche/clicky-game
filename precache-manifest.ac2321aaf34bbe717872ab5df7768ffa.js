090d2465a9c30cc94c55ac3db6c4472ce83b4823
Nicole Apsche <napsche@kgtiger.com>
2020-08-22 11:44:23 -0400

    Updates

---
+++ precache-manifest.ac2321aaf34bbe717872ab5df7768ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9289e67d0df77d68fe4182b5e73b133",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1871c602695afe46d5c6",
+    "url": "/clicky-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae00459dc451066c22ae",
+    "url": "/clicky-game/static/js/2.dbc5170f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.dbc5170f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1871c602695afe46d5c6",
+    "url": "/clicky-game/static/js/main.808104ee.chunk.js"
+  },
+  {
+    "revision": "72ea6c2bbac83c6c626a",
+    "url": "/clicky-game/static/js/runtime-main.b5cfe2d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
